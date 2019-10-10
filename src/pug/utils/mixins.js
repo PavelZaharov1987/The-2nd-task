a3d49fffba8da75c075da0656a9488c45e639ba1
PavelZakharov <pavelzakharov1987@gmail.com>
2019-10-10 23:35:15 +0300

    Added jquery for expandable checkbox list

--- src/pug/utils/mixins.js
+++ src/pug/utils/mixins.js
@@ -12,3 +12,21 @@ $(function() {
         $(this).toggleClass('open');
     });
 });
+
+var res = $(".dropdown-content");
+$(".dropbtn").on("click", funk);
+
+$(document).click(function(e) {
+  if ($(e.target).closest(res).length || $(e.target).closest('.dropbtn').length) return;
+  res.fadeOut(100);
+  e.stopPropagation();
+});
+
+function funk(){
+  if(res.css("display") == "none"){
+    res.fadeIn(100);
+  }
+  else{
+    res.fadeOut(100);
+  }
+}
