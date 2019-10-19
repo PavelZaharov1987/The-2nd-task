454830b4c110fb122fe70c016a4790e9b1b73f68
PavelZakharov <pavelzakharov1987@gmail.com>
2019-10-20 00:21:23 +0300

    Improved BEM structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import './js/'
 
 //mixins.js
 import './pug/utils/mixins'
-// import './pug/includes/components/logo/index'
+import './pug/includes/components/logo'
+import './pug/includes/components/btn'
+import './pug/includes/components/field'
+import './pug/includes/components/dropdown'
+import './pug/includes/components/date-dropdown'
 
 // SCSS
 import './assets/scss/main.scss'
