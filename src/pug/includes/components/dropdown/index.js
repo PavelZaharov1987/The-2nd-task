bcf0130912cf07846b6fcb1865b2b8e312d9fb53
PavelZakharov <pavelzakharov1987@gmail.com>
2019-11-04 01:26:57 +0300

    Added style and js for dropdown, not well

--- src/pug/includes/components/dropdown/index.js
+++ src/pug/includes/components/dropdown/index.js
@@ -14,3 +14,18 @@ $(function() {
         $(this).toggleClass('open');
     });
 });
+
+$('.minus').click(function () {
+  var $input = $(this).parent().find('input');
+  var count = parseInt($input.val()) - 1;
+  count = count < 0 ? 0 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
+$('.plus').click(function () {
+  var $input = $(this).parent().find('input');
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
